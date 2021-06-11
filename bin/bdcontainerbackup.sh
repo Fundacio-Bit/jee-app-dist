b3e03fe6dc054b620c8c0d96bc52e76f9d4c8d43
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2021-06-11 02:03:16 +0200

    Còpia de seguretat amb contenidors #5

---
+++ bin/bdcontainerbackup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#### Description: Backup database using pg_dump
+#### Written by: Guillermo de Ignacio - gdeignacio@fundaciobit.org on 04-2021
+
+###################################
+###   BACKUP UTILS              ###
+###################################
+
+echo ""
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+echo "Project path at $PROJECT_PATH"
+echo ""
+echo "[$(date +"%Y-%m-%d %T")] Saving database..."
+echo ""
+
+# Taking values from .env file
+source $PROJECT_PATH/bin/loadenv.sh
+
+echo Saving container bd with $SCHEMA_ONLY option
+sudo docker exec -i ${LONG_APP_NAME_LOWER}-pg pg_dump \
+    $SCHEMA_ONLY \
+    --file=${PG_DUMP_FILENAME} \
+    --format=t \
+    --verbose \
+    --schema=${PG_DUMP_SCHEMA} \
+    --column-inserts \
+    --host=${PG_DUMP_HOSTNAME} \
+    --port=${PG_DUMP_PORT} \
+    --username=${PG_DUMP_NAME} \
+    ${PG_DUMP_DBNAME}
---
+++ bin/bdcontainerrestore.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#### Description: Restoring database using pg_restore
+#### Written by: Guillermo de Ignacio - gdeignacio@fundaciobit.org on 04-2021
+
+###################################
+###   RESTORE UTILS             ###
+###################################
+
+echo ""
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+echo "Project path at $PROJECT_PATH"
+echo ""
+echo "[$(date +"%Y-%m-%d %T")] Restoring database..."
+echo ""
+
+# Taking values from .env file
+source $PROJECT_PATH/bin/loadenv.sh
+
+echo Restoring container bd with $SCHEMA_ONLY option
+sudo docker exec -i ${LONG_APP_NAME_LOWER}-pg pg_restore \
+    --file=${PG_DUMP_FILENAME} \
+    --format=t \
+    --verbose \
+    --schema=${PG_DUMP_SCHEMA} \
+    --column-inserts \
+    --host=${PG_DUMP_HOSTNAME} \
+    --port=${PG_DUMP_PORT} \
+    --username=${PG_DUMP_NAME} \
+    ${PG_DUMP_DBNAME}
