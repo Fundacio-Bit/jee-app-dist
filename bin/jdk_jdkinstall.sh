277377511c8585b9f74ff74351d64a5d1f0541ad
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2021-07-12 15:45:00 +0200

    Instalacio jdk windows/linux #3

--- bin/jdk_jdkinstall.sh
+++ bin/jdk_jdkinstall.sh
@@ -17,12 +17,26 @@ echo ""
 echo "[$(date +"%Y-%m-%d %T")] Installing JDK..."
 echo ""
 
-source $PROJECT_PATH/bin/lib_string_utils.sh
+source $PROJECT_PATH/bin/lib_string_utils.sh 
 source $PROJECT_PATH/bin/lib_env_utils.sh
 
 lib_env_utils.loadenv ${PROJECT_PATH}
+echo ""
+lib_env_utils.check_os
+echo ""
 
+if [[ isLinux -eq 1 ]]; then
+    JDK_FILE=${JDK_LINUX_FILE}
+else
+    JDK_FILE=${JDK_WINDOWS_FILE}
+fi  
+
+JDK_URL=${JDK_BASEURL}${JDK_FILE}
+
+
+echo ""
 echo "Downloading" $JDK_URL "to" $JDK_TARGET
+echo ""
 wget $JDK_URL -P $JDK_TARGET
-tar -zxvf $JDK_TARGET/$JDK_TARFILE --directory $JDK_TARGET
-rm $JDK_TARGET/$JDK_TARFILE
+tar -zxvf $JDK_TARGET/$JDK_FILE --directory $JDK_TARGET
+rm $JDK_TARGET/$JDK_FILE
