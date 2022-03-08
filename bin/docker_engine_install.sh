16f14ccde3d265f4b0b2cfb23bc340074a9afed5
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2022-03-08 15:52:37 +0100

    Actualitzacio Docker engine #11

--- bin/docker_engine_install.sh
+++ bin/docker_engine_install.sh
@@ -31,19 +31,36 @@ echo ""
 if [[ isLinux -eq 1 ]]; then
     sudo apt-get update
     sudo apt-get upgrade
+    sudo apt-get autoremove
+    
     sudo apt-get install ca-certificates curl gnupg lsb-release
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
+    
     sudo apt-get remove docker docker-engine docker.io docker-compose docker-ce docker-ce-cli containerd.io
     sudo apt-get autoremove
-    sudo apt-get install docker.io docker-compose zip
-    sudo systemctl enable docker
-    sudo systemctl start docker
-    sudo systemctl status docker
-    sudo useradd -p $(openssl passwd -1 docker) docker -g docker
-    # sudo usermod -a -G docker emiserv
-    sudo usermod -a -G docker ${DOCKER_CUSTOM_USERNAME}
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose zip
+
+    if [ $(getent group docker) ]; then
+        echo -e "group docker already exists: skipping group creation"
+    else
+        echo- e "group docker does not exist: adding group"
+        sudo groupadd docker
+    fi
+
+    sudo usermod -aG docker $USER
+    newgrp docker
+
+    echo "starting"
+    sudo systemctl enable docker.service
+    sudo systemctl enable containerd.service
+    sudo systemctl start docker.service
+    sudo systemctl start containerd.service
+    sudo systemctl status docker.service
+    # sudo useradd -p $(openssl passwd -1 docker) docker -g docker
+    # # sudo usermod -a -G docker emiserv
+    # sudo usermod -a -G docker ${DOCKER_CUSTOM_USERNAME}
     sudo mkdir -p /app/docker
 else
     echo ""
