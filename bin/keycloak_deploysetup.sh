b505c887c8794f0f7c5904781e1bf83b3c71baa9
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2023-06-08 22:58:30 +0200

    Configurar certificat de segell al keycloak per https #16

--- bin/keycloak_deploysetup.sh
+++ bin/keycloak_deploysetup.sh
@@ -22,31 +22,65 @@ source $PROJECT_PATH/bin/lib_env_utils.sh
 
 lib_env_utils.loadenv ${PROJECT_PATH}
 
-CFPATH=$KEYCLOAK_CONF_PATH
-echo "Processing: "$CFPATH
-TEMPLATE_FOLDER=$CFPATH.template.d
-SETTINGS_FOLDER=$CFPATH
+CONF_PATH_ARRAY=($KEYCLOAK_DEPLOYCONF_PATH $KEYCLOAK_BIN_CLI_PATH)
+# echo $JBOSS_EAP_52_CONF_PATH
+for CFPATH in ${CONF_PATH_ARRAY[*]}; do
+    echo ""
+    echo "Processing folder: "$CFPATH
+    #TEMPLATE_FOLDER=$DEPLOY_CONF_PATH.template.d
+    #SETTINGS_FOLDER=$DEPLOY_CONF_PATH
+    TEMPLATE_FOLDER=$CFPATH.template.d
+    SETTINGS_FOLDER=$CFPATH
 
+    mkdir -p $SETTINGS_FOLDER
+    echo "Procesing template folder: $TEMPLATE_FOLDER"
+    echo "Configuration at $KEYCLOAK_DEPLOYCONF_PATH"
+    echo ""
 
-if [ "$KEYCLOAK_IMPORT_REALM_JSON" == "" ]; then
-    # Deploy dir value is empty
-    echo  =================================================================
-    echo    Definex la variable d\'entorn KEYCLOAK_IMPORT_REALM_JSON apuntant al
-    echo    directori de configuracio del JBOSS  
-    echo  =================================================================
-else
-    if [[ -f "$TEMPLATE_FOLDER/$KEYCLOAK_IMPORT_REALM_JSON" ]]; then
-        echo Copying $TEMPLATE_FOLDER/$KEYCLOAK_IMPORT_REALM_JSON to $KEYCLOAK_CONF_PATH
-        mkdir -p $SETTINGS_FOLDER
-        cp $TEMPLATE_FOLDER/$KEYCLOAK_IMPORT_REALM_JSON $SETTINGS_FOLDER/import-goib-realm.json
-    else 
-        if [[ -f "${TEMPLATE_FOLDER}/import-goib-realm.json" ]]; then
-            echo Copying default $TEMPLATE_FOLDER/import-goib-realm.json to $KEYCLOAK_CONF_PATH
-            mkdir -p $SETTINGS_FOLDER
-            cp $TEMPLATE_FOLDER/import-goib-realm.json $SETTINGS_FOLDER/import-goib-realm.json
+    for FILE in $TEMPLATE_FOLDER/*; do
+
+        if [ "$TEMPLATE_FOLDER" == "$KEYCLOAK_DEPLOYCONF_PATH.template.d" ]; then
+            
+            if [ "$KEYCLOAK_IMPORT_REALM_JSON" == "" ]; then
+                # Deploy dir value is empty
+                echo  =================================================================
+                echo    Definex la variable d\'entorn KEYCLOAK_IMPORT_REALM_JSON apuntant al
+                echo    directori de configuracio del JBOSS  
+                echo  =================================================================
+            else
+                echo "Realm json file $KEYCLOAK_IMPORT_REALM_JSON as json for $FILE " 
+
+                if [[ -f "${TEMPLATE_FOLDER}/import-goib-realm.json" ]]; then
+                    echo Copying default $TEMPLATE_FOLDER/import-goib-realm.json to $KEYCLOAK_DEPLOYCONF_PATH
+                    mkdir -p $SETTINGS_FOLDER
+                    cp $TEMPLATE_FOLDER/import-goib-realm.json $SETTINGS_FOLDER/import-goib-realm.json
+                fi
+
+                if [[ -f "$TEMPLATE_FOLDER/$KEYCLOAK_IMPORT_REALM_JSON" ]]; then
+                    rm $SETTINGS_FOLDER/import-goib-realm.json
+                    echo Copying $TEMPLATE_FOLDER/$KEYCLOAK_IMPORT_REALM_JSON to $KEYCLOAK_DEPLOYCONF_PATH
+                    mkdir -p $SETTINGS_FOLDER
+                    cp $TEMPLATE_FOLDER/$KEYCLOAK_IMPORT_REALM_JSON $SETTINGS_FOLDER/import-goib-realm.json        
+                fi
+
+            fi
+
+        else
+            echo ""
+            echo "Loading: "$FILE
+            echo ""
+            MASK=${FILE%.template}
+            FILENAME=${MASK##*/}
+            NEWFILE=$SETTINGS_FOLDER/$FILENAME
+            echo "Transforming filename from $FILE to $NEWFILE"
+            cp $FILE $NEWFILE
         fi
-    fi
-fi
+
+       
+
+    done
+
+done
 
 
 echo ""
