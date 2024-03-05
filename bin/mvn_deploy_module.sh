c4ae0b0aecb2d06348f1c2ba9835f3de2158e4b8
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2024-03-05 13:56:25 +0100

    Desplegar mòdul #33

---
+++ bin/mvn_deploy_module.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+#### Description: Build from source
+#### Written by: Guillermo de Ignacio - gdeignacio@fundaciobit.org on 04-2021
+
+###################################
+###   BUILD MVN UTILS           ###
+###################################
+
+echo ""
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+echo "Project path at $PROJECT_PATH"
+echo ""
+echo "[$(date +"%Y-%m-%d %T")] Build and deploy project..."
+echo ""
+
+# Taking values from .env file
+source $PROJECT_PATH/bin/lib_string_utils.sh
+source $PROJECT_PATH/bin/lib_env_utils.sh
+
+lib_env_utils.loadenv ${PROJECT_PATH}
+
+echo off
+if [[ -f help.txt ]]
+then
+  cat help.txt
+else
+  echo "help.txt no existe"
+fi
+
+# Array to compile
+POM_ARRAY=($1)
+shift;
+
+
+# POM compile section
+for POM in ${POM_ARRAY[*]}; do
+  if [[ -f "$POM" ]]
+  then
+      echo "Compiling $POM"
+      env mvn -f $POM -DskipTests $@ deploy -Dmaven.wagon.http.ssl.insecure=true \
+        --settings $PROJECT_PATH/builds/maven-dist/maven/conf/settings.xml \
+        --toolchains $PROJECT_PATH/builds/maven-dist/maven/conf/toolchains.xml
+  fi
+done
+# end of POM compile section
+
+
