ee99221ab731f0eb466494fa6a478ca32b0eb8b3
gdeignacio-fundaciobit <gdeignacio@fundaciobit.org>
2024-03-13 11:21:45 +0100

    Executar nginx dins el contenidor #8

---
+++ bin/docker_exec_nginx.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
+#### Description: Runs webapp docker container
+#### Written by: Guillermo de Ignacio - gdeignacio@fundaciobit.org on 04-2021
+
+###################################
+###         Exec                ###
+###################################
+
+echo ""
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+echo "Project path at $PROJECT_PATH"
+echo ""
+echo "[$(date +"%Y-%m-%d %T")] Launching web server..."
+echo ""
+
+source $PROJECT_PATH/bin/lib_string_utils.sh 
+source $PROJECT_PATH/bin/lib_env_utils.sh
+
+lib_env_utils.loadenv ${PROJECT_PATH}
+echo ""
+lib_env_utils.check_os
+echo ""
+lib_env_utils.check_docker
+echo ""
+
+if [[ "${DOCKER}" == "/dev/null" ]]; then
+  echo "Docker not installed. Exiting"
+  exit 1
+fi
+
+${DOCKER} exec -i -t nginx-${APP_PROJECT_DOCKER_SERVER_NAME} /bin/bash
